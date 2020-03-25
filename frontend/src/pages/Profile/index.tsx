078c91a5984724850cdfbfd332f3eb430ff448e9
Heitor <hbanhete@gmail.com>
2020-03-25 11:45:59 -0300

    Created pages and synced with API

---
+++ frontend/src/pages/Profile/index.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiPower, FiTrash } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css';
+
+import api from '../../services/api';
+
+export default function Profile () {
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+
+  const [incidents, setIncidents] = useState<any[]>([]);
+
+  useEffect(() => {
+    api.get('/profile', {
+      headers: { 
+        Authorization: ongId
+      }
+    }).then(response => {
+      setIncidents(response.data.incidents);
+    });
+  }, [ongId]);
+
+  return (
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt='Be The Hero'/>
+        <span>Bem vinda, {ongName}</span>
+
+        <Link className='button' to='/incidents/new'> Cadastrar novo caso </Link>
+        <button type='button' onClick={handleLogout}>
+          <FiPower size={18} color='#E02041' />
+        </button>
+      </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        {incidents.map((incident: any) => (
+          <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
+
+            <strong>DESCRIÇÃO:</strong>
+            <p>{incident.description}</p>
+
+            <strong>VALOR: </strong>
+            <p>{Intl.NumberFormat('pt-BR', {  style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
+
+            <button type='button' onClick={() => handleDeleteIncident(incident.id)}>
+              <FiTrash size={20} color='#a8a8b3' />
+            </button>
+         </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  async function handleDeleteIncident(id: any) {
+    try {
+      await api.delete(`/incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+
+    } catch (err) {
+      alert('Erro ao deletar caso');
+    }
+  }
+
+  function handleLogout () {
+    localStorage.clear();
+    history.push('/');
+  }
+}
